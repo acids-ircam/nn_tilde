9bc4e76d0a93ed7564462b7804d70fc4c9d78045
Antoine <caillonantoine@gmail.com>
2022-05-31 10:36:03 +0200

    cleanup output

--- install/raspberrypi.sh
+++ install/raspberrypi.sh
@@ -1,17 +1,27 @@
 # INSTALL DEPENDENCIES
-sudo apt update
-sudo apt install -y cmake build-essential git puredata puredata-dev python3 python3-pip
-python3 -m pip install torch
+echo "installing compilation tools..."
+sudo apt update > /dev/null 2>&1
+sudo apt install -y cmake build-essential git puredata puredata-dev python3 python3-pip > /dev/null 2>&1
+
+echo "installing libtorch..."
+python3 -m pip install torch > /dev/null 2>&1
 LIBTORCH=$(pwd)/.local/lib/python3.9/site-packages/torch/
 
 # INSTALL NN TILDE
-git clone https://github.com/acids-ircam/nn_tilde.git
+echo "configuring nn~..."
+git clone https://github.com/acids-ircam/nn_tilde.git > /dev/null 2>&1
 cd nn_tilde
 mkdir build
 cd build
-cmake ../src/ -DCMAKE_PREFIX_PATH=$LIBTORCH -DCMAKE_BUILD_TYPE=Release
-make
+cmake ../src/ -DCMAKE_PREFIX_PATH=$LIBTORCH -DCMAKE_BUILD_TYPE=Release > /dev/null 2>&1
+
+echo "compiling nn~..."
+make > /dev/null 2>&1
 sudo mkdir -p /usr/local/lib/pd-externals/
+
+echo "installing nn~..."
 sudo cp frontend/puredata/nn_tilde/nn~.pd_linux /usr/local/lib/pd-externals/
 cd ../../
 rm -fr nn_tilde
+
+echo "done !"
