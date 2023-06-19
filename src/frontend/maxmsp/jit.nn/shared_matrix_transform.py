50739bb5234c44c301b2819e7633e6aa8365c4b0
Antoine <caillonantoine@gmail.com>
2023-06-19 16:09:06 +0200

    update smt

--- src/frontend/maxmsp/jit.nn/shared_matrix_transform.py
+++ src/frontend/maxmsp/jit.nn/shared_matrix_transform.py
@@ -1,11 +1,12 @@
+import hashlib
+from collections import OrderedDict
+from threading import Thread
 from time import sleep, time
+from typing import Callable, Optional, Sequence
 
 import torch
 import torch.nn as nn
 from torch.utils import cpp_extension
-from collections import OrderedDict
-import hashlib
-from typing import Optional, Sequence
 
 torch.set_grad_enabled(False)
 
@@ -17,18 +18,60 @@ shared_tensor_lib = cpp_extension.load(
 )
 
 
+class BufferedComputation:
+
+    def __init__(self, batch_size: int,
+                 apply_fun: Callable[[torch.Tensor], torch.Tensor]) -> None:
+        self.input_buffer = []
+        self.output_buffer = []
+        self.result = None
+        self.apply_fun = apply_fun
+        self.batch_size = batch_size
+        self.thread = None
+
+    def __call__(self, x: torch.Tensor) -> torch.Tensor:
+        self.input_buffer.append(x)
+
+        if len(self.input_buffer) >= self.batch_size:
+            input_buffer = torch.stack(self.input_buffer, 0)
+            self.input_buffer = []
+            if self.thread is not None:
+                self.thread.join()
+                self.output_buffer.extend(list(self.result))
+            self.thread = Thread(target=threaded_forward,
+                                 args=(self, input_buffer))
+            self.thread.start()
+
+        if len(self.output_buffer) < self.batch_size:
+            return torch.zeros_like(x)
+        else:
+            return self.output_buffer.pop(0)
+
+
+def threaded_forward(buffered_class: BufferedComputation,
+                     input_buffer: torch.Tensor) -> torch.Tensor:
+    output_buffer = buffered_class.apply_fun(input_buffer)
+    buffered_class.result = output_buffer.cpu()
+
+
 class SharedMatrixTransform(nn.Module):
 
-    def __init__(self,
-                 cache_len: int = 4,
-                 rescale: Optional[Sequence[int]] = None) -> None:
+    def __init__(
+        self,
+        rescale: Optional[Sequence[int]] = None,
+        batch_size: int = 1,
+    ) -> None:
         super().__init__()
         self.cache = OrderedDict()
-        self.cache_len = cache_len
         self.permute = torch.Tensor([[0, 0, 1], [1, 0, 0], [0, 1, 0]])
         self.rescale = rescale
         if self.rescale is not None:
             self.rescale = nn.Upsample(size=rescale)
+
+        self.buffer = BufferedComputation(batch_size=batch_size,
+                                          apply_fun=self.forward)
+
+        self.previous_frame_hash = None
 
     def start(self, name: str, fps: int) -> None:
         try:
@@ -56,37 +99,40 @@ class SharedMatrixTransform(nn.Module):
             except RuntimeError:
                 raise RuntimeError(
                     f"Couldn't find shared stream with name {stream_name}")
+
             tensor_hash = hashlib.md5(
                 input_tensor.numpy().tobytes()).hexdigest()
-            if tensor_hash in self.cache:
-                output_tensor = self.cache[tensor_hash]
+            if self.previous_frame_hash is None:
+                self.previous_frame_hash = tensor_hash
+            elif self.previous_frame_hash == tensor_hash:
+                pass
             else:
-                output_tensor = self.forward(input_tensor)
-                if len(self.cache) == self.cache_len:
-                    self.cache.popitem(last=False)
-                self.cache[tensor_hash] = output_tensor
+                self.previous_frame_hash = tensor_hash
+                output_tensor = self.buffer(input_tensor)
 
-            if output_tensor.shape != input_tensor.shape:
-                raise ValueError(
-                    f"Inconsistent shapes, got {input_tensor.shape} -> {output_tensor.shape} "
-                )
-            else:
-                try:
-                    stream_name = f"{name}_output"
-                    if self.rescale is not None:
-                        output_tensor = output_tensor.permute(2, 0, 1)[None]
-                        output_tensor = nn.functional.interpolate(
-                            output_tensor,
-                            size=original_size,
-                            recompute_scale_factor=self.rescale.
-                            recompute_scale_factor,
+                if output_tensor.shape != input_tensor.shape:
+                    raise ValueError(
+                        f"Inconsistent shapes, got {input_tensor.shape} -> {output_tensor.shape} "
+                    )
+                else:
+                    try:
+                        stream_name = f"{name}_output"
+                        if self.rescale is not None:
+                            output_tensor = output_tensor.permute(2, 0,
+                                                                  1)[None]
+                            output_tensor = nn.functional.interpolate(
+                                output_tensor,
+                                size=original_size,
+                                recompute_scale_factor=self.rescale.
+                                recompute_scale_factor,
+                            )
+                            output_tensor = output_tensor[0].permute(1, 2, 0)
+
+                        shared_tensor.set_shared_tensor(output_tensor)
+                    except RuntimeError:
+                        raise RuntimeError(
+                            f"Couldn't find shared stream with name {stream_name}"
                         )
-                        output_tensor = output_tensor[0].permute(1, 2, 0)
-
-                    shared_tensor.set_shared_tensor(output_tensor)
-                except RuntimeError:
-                    raise RuntimeError(
-                        f"Couldn't find shared stream with name {stream_name}")
 
             current_dt = time() - start_time
 
