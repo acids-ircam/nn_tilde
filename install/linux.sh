ee2a8cfc8c1cb4db036293af38d33357295e1f74
Martin Heinze <47271992+devstermarts@users.noreply.github.com>
2023-09-08 23:13:53 +0200

    libtorch version: nighty -> latest stable
    Build with most recent nightly version failing. Replaced with latest stable version.

--- install/linux.sh
+++ install/linux.sh
@@ -1,6 +1,6 @@
 # Install dependencies
 sudo apt update
-sudo apt install -y cmake build-essential git puredata puredata-dev python3 python3-pip
+sudo apt install -y cmake build-essential git puredata puredata-dev python3 python3-pip curl
 
 # Download and install nn_tilde
 git clone https://github.com/acids-ircam/nn_tilde
@@ -9,9 +9,9 @@ cd nn_tilde
 # Download and install libtorch
 mkdir lib
 cd lib
-curl -LO https://download.pytorch.org/libtorch/nightly/cpu/libtorch-shared-with-deps-latest.zip
-unzip libtorch-shared-with-deps-latest.zip
-rm -rf libtorch-shared-with-deps-latest.zip
+curl -LO https://download.pytorch.org/libtorch/cpu/libtorch-shared-with-deps-2.0.1%2Bcpu.zip
+unzip libtorch-shared-with-deps-2.0.1%2Bcpu.zip
+rm -rf libtorch-shared-with-deps-2.0.1%2Bcpu.zip
 cd ../
 
 # Setup Build
