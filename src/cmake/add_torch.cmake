215c784b5c3199c950d7f01f3e220e1471888e4b
Axel Chemla--Romeu-Santos <axelchemla@yahoo.fr>
2025-04-23 11:30:19 +0200

    forcing lib path

--- src/cmake/add_torch.cmake
+++ src/cmake/add_torch.cmake
@@ -45,34 +45,56 @@ if (NEEDS_DL)
   if (UNIX AND NOT APPLE)  # Linux
     set(torch_url "https://download.pytorch.org/libtorch/cpu/libtorch-cxx11-abi-shared-with-deps-${torch_version}%2Bcpu.zip")
     download_library(${torch_url} ${torch_dir})
-    #message("Please install torch in /usr/local/lib before making ; see README or consult build_linux script")
-  elseif (UNIX AND APPLE)  # OSXx
-    # Check if architecutre == ARM64
-    if (NOT DEFINED APPLE_ARM64)
-      set (APPLE_ARM64 (CMAKE_SYSTEM_PROCESSOR STREQUAL "arm64"))
-    endif()
-    ## If ARM, download both libraries and pre-compile UB libraries
-    if (APPLE_ARM64)
-      # download arm64 library
-      if (NOT IS_DIRECTORY ${torch_dir}-arm64)
-        download_library("https://anaconda.org/pytorch/pytorch/${torch_version}/download/osx-arm64/pytorch-${torch_version}-py3.10_0.tar.bz2" ${torch_dir}-arm64)
-        execute_process(COMMAND mkdir ${torch_dir})
-        execute_process(COMMAND cp -r ${torch_dir}-arm64/lib/python3.10/site-packages/torch ${torch_dir}/libtorch)
-      endif()
-      # download x86 library
-      # download_library("https://download.pytorch.org/libtorch/cpu/libtorch-macos-${torch_version}.zip" ${torch_dir}-x86)
-      # export UB libs to main path
-      execute_process(COMMAND mkdir ${torch_dir}-x86)
-      if (NOT EXISTS ${CMAKE_CURRENT_SOURCE_DIR}/Extras/macos/torch_x86/libtorch)
-        execute_process(COMMAND cp -r ${CMAKE_CURRENT_SOURCE_DIR}/Extras/macos/torch_x86 ${torch_dir}-x86/libtorch)
-      endif()
-      execute_process(COMMAND cp ${torch_dir}-x86/libtorch/lib/libomp.dylib ${torch_dir}/libtorch/lib)
-      execute_process(COMMAND cp /opt/homebrew/opt/llvm/lib/libomp.dylib ${torch_dir}-x86/libtorch/lib/)
-      execute_process(COMMAND find ${torch_dir}/libtorch/lib -maxdepth 1 -type f -execdir lipo -create ${torch_dir}/libtorch/lib/{} ${torch_dir}-x86/libtorch/lib/{} -output ${torch_dir}/libtorch/lib/{} \;)
-      execute_process(COMMAND rm -rf ${torch_dir}-x86)
-      file(REMOVE ${torch_dir}-x86)
+  elseif (UNIX AND APPLE)  # OSX
+    if (NOT IS_DIRECTORY ${torch_dir})
+    if (EXISTS ${CMAKE_SOURCE_DIR}/../install/torch_ub)
+      execute_process(COMMAND cp -r ${CMAKE_SOURCE_DIR}/../install/torch_ub ${torch_dir})
     else()
-      
+      if (NOT DEFINED TORCH_MAC_UB_URL)
+        message(FATAL_ERROR "If not provided, please give a valid URL for Apple universal library with -DTORCH_MAC_UB_URL ")
+      endif()
+      download_library(${TORCH_MAC_UB_URL} /tmp)
+      execute_process(
+        COMMAND mv /tmp/torch_ub ${torch_dir}
+      )
+    endif()
+
+    # Check if architecutre == ARM64
+    # if (NOT DEFINED APPLE_ARM64)
+    #   set (APPLE_ARM64 (CMAKE_SYSTEM_PROCESSOR STREQUAL "arm64"))
+    # endif()
+    ## If ARM, download both libraries and pre-compile UB libraries
+    # if (APPLE_ARM64)
+      # download arm64 library
+      # if (NOT IS_DIRECTORY ${torch_dir})
+      #   download_library("https://anaconda.org/pytorch/pytorch/${torch_version}/download/osx-arm64/pytorch-${torch_version}-py3.10_0.tar.bz2" ${torch_dir}-arm64)
+      #   execute_process(COMMAND mkdir ${torch_dir})
+      #   execute_process(COMMAND cp -r ${torch_dir}-arm64/lib/python3.10/site-packages/torch ${torch_dir}/libtorch)
+      # endif()
+      # # download x86 library
+      # if (EXISTS ${CMAKE_SOURCE_DIR}/../install/torch_x86)
+      #   execute_process(COMMAND cp -r ${CMAKE_SOURCE_DIR}/../install/torch_x86 ${torch_dir}-x86)
+      # else()
+      #   if (NOT DEFINED TORCH_MAC_UB_URL)
+      #     message(FATAL_ERROR "If not provided, please give a valid URL for Apple universal library")
+      #   endif()
+      #   download_library(${TORCH_MAC_X86_URL} ${torch_dir})
+      # endif()
+      # message("found libtorch for x86 at : " ${torch_dir}-x86)
+      # # export UB libs to main path
+      # execute_process(COMMAND mkdir ${torch_dir}-x86)
+      # execute_process(COMMAND cp /opt/homebrew/opt/llvm/lib/libomp.dylib ${torch_dir}/libtorch/lib/)
+      # execute_process(COMMAND find ${torch_dir}/libtorch/lib -maxdepth 1 -type f -execdir lipo -create ${torch_dir}/libtorch/lib/{} ${torch_dir}-x86/libtorch/lib/{} -output ${torch_dir}/libtorch/lib/{} \;)
+    # else()
+    #   if (EXISTS ${CMAKE_SOURCE_DIR}/../install/torch_x86)
+    #     execute_process(COMMAND cp ${CMAKE_SOURCE_DIR}/../install/torch_x86 ${torch_dir})
+    #   else()
+    #     if (NOT DEFINED TORCH_MAC_X86_URL)
+    #       message(FATAL_ERROR "If not provided, please give a valid URL for Apple x86 library")
+    #     endif()
+    #     download_library(${TORCH_MAC_X86_URL} ${torch_dir})
+    #   endif()
+    #   message("found libtorch for x86 at : " ${torch_dir})
     endif()
   else()
     execute_process(COMMAND ${CMAKE_COMMAND} -E make_directory ${torch_dir})
