188ca9efed411e64f9cbdc24e68b48f31d1b417e
Axel Chemla--Romeu-Santos <axelchemla@yahoo.fr>
2025-05-18 11:40:53 +0200

    action fixes

--- src/cmake/add_torch.cmake
+++ src/cmake/add_torch.cmake
@@ -40,27 +40,31 @@ endif()
 
 if (NEEDS_DL)
   message(STATUS "Downloading torch C API pre-built")
-
   # Download
   if (UNIX AND NOT APPLE)  # Linux
     set(torch_url "https://download.pytorch.org/libtorch/cpu/libtorch-shared-with-deps-${torch_version}%2Bcpu.zip")
     download_library(${torch_url} ${torch_dir})
   elseif (UNIX AND APPLE)  # OSX
     if (NOT IS_DIRECTORY ${torch_dir})
-    if (EXISTS ${CMAKE_SOURCE_DIR}/../install/torch_ub)
-      execute_process(COMMAND cp -r ${CMAKE_SOURCE_DIR}/../install/torch_ub ${torch_dir})
-    else()
-      if (NOT DEFINED TORCH_MAC_UB_URL)
-        message(FATAL_ERROR "If not provided, please give a valid URL for Apple universal library with -DTORCH_MAC_UB_URL ")
+      if (EXISTS ${CMAKE_SOURCE_DIR}/../install/torch_ub)
+        execute_process(COMMAND cp -r ${CMAKE_SOURCE_DIR}/../install/torch_ub ${torch_dir})
+      elseif(DEFINED TORCH_MAC_UB_URL)
+        download_library(${TORCH_MAC_UB_URL} /tmp)
+        execute_process(
+          COMMAND mkdir -p ${torch_dir}
+          COMMAND ls /tmp
+          COMMAND mv /tmp/torch ${torch_dir}/libtorch
+        )
+      else()
+        execute_process(COMMAND ${CMAKE_COMMAND} -E make_directory ${torch_dir})
+        set(torch_url "https://download.pytorch.org/libtorch/cpu/libtorch-macos-arm64-${torch_version}.zip")
+        download_library(${torch_url} ${torch_dir})
       endif()
-      download_library(${TORCH_MAC_UB_URL} /tmp)
-      execute_process(
-        COMMAND mkdir -p ${torch_dir}
-        COMMAND ls /tmp
-        COMMAND mv /tmp/torch ${torch_dir}/libtorch
-      )
     endif()
-
+  else()
+    execute_process(COMMAND ${CMAKE_COMMAND} -E make_directory ${torch_dir})
+    download_library("https://download.pytorch.org/libtorch/cpu/libtorch-win-shared-with-deps-${torch_version}%2Bcpu.zip" ${torch_dir})
+  endif()
     # Check if architecutre == ARM64
     # if (NOT DEFINED APPLE_ARM64)
     #   set (APPLE_ARM64 (CMAKE_SYSTEM_PROCESSOR STREQUAL "arm64"))
@@ -97,11 +101,7 @@ if (NEEDS_DL)
     #     download_library(${TORCH_MAC_X86_URL} ${torch_dir})
     #   endif()
     #   message("found libtorch for x86 at : " ${torch_dir})
-    endif()
-  else()
-    execute_process(COMMAND ${CMAKE_COMMAND} -E make_directory ${torch_dir})
-    download_library("https://download.pytorch.org/libtorch/cpu/libtorch-win-shared-with-deps-${torch_version}%2Bcpu.zip" ${torch_dir})
-  endif()
+
 endif()
 
 # Find the libraries again
